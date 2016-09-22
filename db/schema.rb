35b88d90a55696a7a00802cf9d4d2bac7a40b53f
fugumagu <jeremy.clarkson@outlook.com>
2016-09-22 14:41:58 -0700

    Added blogs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160921212333) do
+ActiveRecord::Schema.define(version: 20160922195340) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.string   "user_id"
+    t.text     "entry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
