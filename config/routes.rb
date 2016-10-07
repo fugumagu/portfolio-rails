b113e297f00ac7f736f2cabdedbe0505f3ab9b3b
fugumagu <jeremy.clarkson@outlook.com>
2016-10-06 17:00:38 -0700

    Added edit and delete on blogs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   resources :blogs
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -7,6 +9,10 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get '/portfolio/', to: 'static_pages#portfolio'
   get '/contact/', to: 'static_pages#contact'
+  get '/blogapp/', to: 'static_pages#blogapp'
+  get '/login/', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/login', to: 'sessions#destroy'
 
 
 end
